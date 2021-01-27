f6ebba8e6a7f3dcbedfeb0215e78c2bcf77eca9e
ghdwpaks <hjm7904@gmail.com>
2021-01-27 17:23:40 +0900

    first commit

---
+++ back_to_java_coder/main1_simples.java
@@ -0,0 +1,28 @@
+package back_to_java_coder;
+import java.util.*;
+
+public class main1_simples {
+	
+	
+	public static void func1() {
+		System.out.println("here is func1");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("program started!");
+		String a;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a >>");
+		a = sc.next();
+		System.out.println("a = " + a);
+		
+		String abc;
+		System.out.print("abc >>");
+		abc = sc.next();
+		System.out.println("abc = " + abc);
+		
+		
+
+	}
+
+}
