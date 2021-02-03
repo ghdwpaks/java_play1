01871a513a372adeee62cff657164b3a2cfd510a
ghdwpaks <hjm7904@gmail.com>
2021-02-03 23:31:13 +0900

    how to just do sort with java

--- back_to_java_coder/main7_sort.java
+++ back_to_java_coder/main7_sort.java
@@ -4,7 +4,7 @@ package back_to_java_coder;
 public class main7_sort {
 
 	//
-	public static void func14_sort_shower(int a[]) {
+	public static void func14_sort_int_shower(int a[]) {
         int size = a.length;
         for(int i=size-1; i>0; i--) {
         	System.out.printf("\n%dÂ÷ Á¤·Ä : ", size-i);
@@ -19,6 +19,16 @@ public class main7_sort {
             }            
         }
         System.out.println();
+    }
+	
+	public static int[] func14_sort_int_blind(int a[]) {
+        int size = a.length;
+        for(int i=size-1; i>0; i--) {
+            for(int j=0; j<i; j++) {
+                if(a[j] > a[j+1])func14_1_swap(a,j,j+1);
+            }            
+        }
+        return a;
     }
 
 	public static void func14_1_swap(int a[], int idx1, int idx2) {
@@ -39,10 +49,14 @@ public class main7_sort {
 		int[] idx = new int[userans1];
 		for (int i = 0; i < userans1; i++) {
 			idx[i] = main3_simples.func8_only_num(idx[i], "idx["+i+"]");
+			//System.out.println("idx["+i+"] = " + idx[i]);
+		}
+		System.out.println("\n===\n");
+		//func14_sort_int_shower(idx);
+		idx = func14_sort_int_blind(idx);
+		for (int i = 0; i < idx.length; i++) {
 			System.out.println("idx["+i+"] = " + idx[i]);
 		}
-		func14_sort_shower(idx);
-		
 		
 	}
 
